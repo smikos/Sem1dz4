b5c12cb5fab62d5157a380c1d69c3b73beefad01
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-14 21:29:09 +0300

    work good

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число =>");
+string inputedString = Console.ReadLine();  
+int number = Convert.ToInt32(inputedString);
+int num = number-(number-1);
+
+while (num <number+1)
+{
+   int c = (num%2);
+    if ( c == 0)
+    {
+      Console.WriteLine(num);
+      num=num+1;  
+    }
+    else
+    num=num+1;
+}
